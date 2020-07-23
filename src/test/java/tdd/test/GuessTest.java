8f9584720f350eed140b2c69bf386f7f626d3598
KEANU.WONG <386247915@qq.com>
2020-07-23 20:29:20 +0800

    [Guess][KEANU] test should_output_4A0B_when_guess_four_numbers_given_1234 fail

--- src/test/java/tdd/test/GuessTest.java
+++ src/test/java/tdd/test/GuessTest.java
@@ -14,4 +14,15 @@ public class GuessTest {
         //then
         Assertions.assertEquals("4A0B",result);
     }
+
+    @Test
+    public void should_output_0A0B_when_guess_four_numbers_given_5678(){
+        //given
+        int[] input={5,6,7,8};
+        Guess guess=new Guess();
+        //when
+        String result=guess.compareTheInputAndAnswer(input);
+        //then
+        Assertions.assertEquals("4A0B",result);
+    }
 }
