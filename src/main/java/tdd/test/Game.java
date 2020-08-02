1a4b2201f360274ed273228ae01e7a1a78bd97a5
KEANU.WONG <386247915@qq.com>
2020-08-02 11:45:00 +0800

    [GuessNumber][KEANU] refactor: refactor the Game class

--- src/main/java/tdd/test/Game.java
+++ src/main/java/tdd/test/Game.java
@@ -1,31 +1,55 @@
 package tdd.test;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Game {
-    public static void main(String[] args){
-        int totalTimes=6;
-        Guess guess=new Guess(new TestAnswerGenerator());
-        int[] answer= guess.getRandomAnswer();
-        System.out.println( "aaa");
-        Scanner scanner=new Scanner(System.in);
+    private Guess guess;
+    private static final String WinDescription="you win!";
+    private static final String FailDescription="game over, try again...";
+    private static final String rightResult="4A0B";
+    public Game(Guess guess){
+        this.guess=guess;
+    }
+
+    public int[] getAnswer(){
+        if(guess!=null)
+        return guess.getRandomAnswer();
+        return null;
+    }
+
+    private int[] generateInput(){
         int[] input=new int[4];
-        System.out.println( Arrays.toString(answer));
-        int begin=0;
-        while (begin<totalTimes){
-            for(int i=0;i<4;i++){
-                input[i]=scanner.nextInt();
-            }
-            String result=guess.compareTheInputAndAnswer(input, answer);
-            if(result.equals("4A0B")){
-                System.out.println("you win!");
+        Scanner scanner=new Scanner(System.in);
+        for(int i=0;i<4;i++){
+            input[i]=scanner.nextInt();
+        }
+        return input;
+    }
+
+    public static void process(){
+        int totalTimes=6;
+        int currentTime=0;
+        Guess guess=new Guess(new TestAnswerGenerator());
+        Game game=new Game(guess);
+        int[] answer=game.getAnswer();
+        while (currentTime<totalTimes){
+            int[] input=game.generateInput();
+            String result=guess.compareTheInputAndAnswer(input,answer);
+            if(result.equals(rightResult)){
                 break;
             }else {
                 System.out.println(result);
             }
-            begin++;
+            currentTime++;
         }
+        if(currentTime==6){
+            System.out.println(Game.FailDescription);
+        }else {
+            System.out.println(Game.WinDescription);
+        }
+    }
 
+    public static void main(String[] args){
+        process();
     }
 }
