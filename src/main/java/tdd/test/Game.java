8e480367a50868f7c14450e833718712f23bb951
KEANU.WONG <386247915@qq.com>
2020-08-02 11:13:14 +0800

    [GuessNumber][KEANU] feature: feature the game class and test it

--- src/main/java/tdd/test/Game.java
+++ src/main/java/tdd/test/Game.java
@@ -1,5 +1,6 @@
 package tdd.test;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Game {
@@ -7,17 +8,23 @@ public class Game {
         int totalTimes=6;
         Guess guess=new Guess(new TestAnswerGenerator());
         int[] answer= guess.getRandomAnswer();
+        System.out.println( "aaa");
         Scanner scanner=new Scanner(System.in);
         int[] input=new int[4];
-        for(int i=0;i<4;i++){
-            System.out.printf(" ", answer[i]);
-        }
+        System.out.println( Arrays.toString(answer));
         int begin=0;
         while (begin<totalTimes){
             for(int i=0;i<4;i++){
                 input[i]=scanner.nextInt();
             }
-            guess.compareTheInputAndAnswer(input,answer);
+            String result=guess.compareTheInputAndAnswer(input, answer);
+            if(result.equals("4A0B")){
+                System.out.println("you win!");
+                break;
+            }else {
+                System.out.println(result);
+            }
+            begin++;
         }
 
     }
